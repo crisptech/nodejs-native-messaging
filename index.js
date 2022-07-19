e41b5ab8dea994fedf7c3492e564797ccada8219
crisptech <crisptech@hotmail.com>
2022-07-19 13:50:17 +0100

    feat: working nodejs native messaging host

---
+++ index.js
@@ -0,0 +1,82 @@
+require("buffer");
+require("./messaging");
+
+let main = () => {
+  let payloadSize = null;
+
+  // A queue to store the chunks as we read them from stdin.
+  // This queue can be flushed when `payloadSize` data has been read
+  let chunks = [];
+
+  // Only read the size once for each payload
+  const sizeHasBeenRead = () => Boolean(payloadSize);
+
+  // All the data has been read, reset everything for the next message
+  const flushChunksQueue = () => {
+    payloadSize = null;
+    chunks.splice(0);
+  };
+
+  const processData = () => {
+    // Create one big buffer with all the chunks
+    const stringData = Buffer.concat(chunks);
+
+    // The browser will emit the size as a header of the payload,
+    // if it hasn't been read yet, do it.
+    // The next time we'll need to read the payload size is when all of the data
+    // of the current payload has been read (ie. data.length >= payloadSize + 4)
+    if (!sizeHasBeenRead()) {
+      payloadSize = stringData.readUInt32LE(0);
+    }
+
+    // If the data we have read so far is >= to the size advertised in the header,
+    // it means we have all of the data sent.
+    // We add 4 here because that's the size of the bytes that old the payloadSize
+    if (stringData.length >= payloadSize + 4) {
+      // Remove the header
+      const contentWithoutSize = stringData.subarray(4, payloadSize + 4);
+
+      // Reset the read size and the queued chunks
+      flushChunksQueue();
+
+      const json = JSON.parse(contentWithoutSize);
+      // Do something with the data…
+    }
+  };
+
+  process.stdin.on("readable", () => {
+    // A temporary variable holding the nodejs.Buffer of each
+    // chunk of data read off stdin
+    let chunk = null;
+    // Read all of the available data
+    while ((chunk = process.stdin.read()) !== null) {
+      chunks.push(chunk);
+    }
+    processData();
+  });
+
+  const sendMessage = (message) => {
+    let bufferHeader = Buffer.alloc(4);
+    let messageStr = JSON.stringify(message);
+
+    var encodedMessageStr = Buffer.from(messageStr, "utf-8").toString();
+
+    bufferHeader.writeUInt32LE(encodedMessageStr.length, 0);
+
+    let bufferMessageContent = Buffer.from(encodedMessageStr);
+
+    let finalMessage = Buffer.concat([bufferHeader, bufferMessageContent]);
+
+    process.stdout.write(finalMessage);
+  };
+
+  return sendMessage;
+};
+
+let sendMessage = main();
+
+sendMessage({ hi: "bye" });
+
+setInterval(() => {
+  sendMessage({ huh: "thats funny" });
+}, 1000);
